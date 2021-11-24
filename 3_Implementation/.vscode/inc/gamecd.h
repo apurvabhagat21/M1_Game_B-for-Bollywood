401456be385c9a1bd59c67994955cf8db90564ae
apurvabhagat21 <74763172+apurvabhagat21@users.noreply.github.com>
2021-11-25 00:33:45 +0530

    Add files via upload

---
+++ 3_Implementation/.vscode/inc/gamecd.h
@@ -0,0 +1,15 @@
+#ifndef _SYSTEM_H
+#define _SYSTEM_H
+#include<stdio.h>
+#include<string.h>
+void menu()
+{
+    printf("\nHELLO LET'S PLAY A GAME\n");
+    printf("\nHere are some rules for the game");
+    printf("\n1.The player will be the initials of hero,heroine,song & movie.");
+    printf("\n2.In order to win the the game player needs to guess all four correct");
+    printf("\n3.The player needs to put the spellings correct");
+    printf("\n4.The player will be given 9 chances");
+    printf("\n5.The player will be provided a hint in between");
+}
+#endif // _SYSTEM_H
